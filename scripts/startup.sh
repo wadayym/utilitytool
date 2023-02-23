2dae340a1621e3c5973d1c438a2bf419ad7f7e58
wadayym <35057133+wadayym@users.noreply.github.com>
2023-02-23 09:05:30 +0900

    import CV2でエラーになる件の対応

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -4,5 +4,5 @@ apt-get -y install tesseract-ocr
 apt-get -y install tesseract-ocr-jpn
 apt-get -y install poppler-utils
 apt-get -y install poppler-data
-apt-get -y install libopencv-dev
+#!apt-get -y install libopencv-dev
 gunicorn --bind=0.0.0.0 --timeout 600 application:app
