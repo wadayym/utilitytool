d8ae0f6ad164413c8f446a3f35c1701aebf57593
wadayym <35057133+wadayym@users.noreply.github.com>
2023-02-27 22:57:47 +0900

    tesseract-ocr-jpnをインストールするようにした
    簡略化してtesseract-ocr-jpnを除くと tesseract is not installedというエラーが発生したので、インストールするようにした。

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 apt-get update
 apt-get -y install tesseract-ocr
-#apt-get -y install tesseract-ocr-jpn
+apt-get -y install tesseract-ocr-jpn
 #apt-get -y install poppler-utils
 #apt-get -y install poppler-data
 gunicorn --bind=0.0.0.0 --timeout 600 application:app
