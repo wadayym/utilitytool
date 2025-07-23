315d1dbd9ebee7e17349493eac8773c42e994c74
wadayym <35057133+wadayym@users.noreply.github.com>
2025-07-23 17:25:46 +0900

    startup.shを修正した。

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,7 +1,2 @@
 #!/bin/sh
-apt-get update
-pip install torch==2.2.2 torchvision==0.17.2 --no-cache-dir
-#apt-get -y install tesseract-ocr-jpn
-#apt-get -y install poppler-utils
-#apt-get -y install poppler-data
 gunicorn --bind=0.0.0.0 --timeout 600 application:app
